c08ffa4ab67b58944e04a89a9143b2ed2a1e87dc
SinigagliaSteeve <steeve.sinigaglia@gmail.com>
2017-11-20 17:27:42 +0100

    download blob implemented

--- src/main/java/com/zenika/zencontact/resource/PhotoResource.java
+++ src/main/java/com/zenika/zencontact/resource/PhotoResource.java
@@ -1,5 +1,6 @@
 package com.zenika.zencontact.resource;
 
+import com.google.appengine.api.blobstore.BlobKey;
 import com.zenika.zencontact.domain.blob.PhotoService;
 
 import javax.servlet.ServletException;
@@ -26,5 +27,19 @@ public class PhotoResource extends HttpServlet {
         PhotoService.getInstance().updatePhoto(id, req);
         resp.setContentType("/text/plain");
         resp.getWriter().println("ok");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pathInfo = req.getPathInfo(); // /{id}
+        String[] pathParts = pathInfo.split("/");
+        if (pathParts.length == 0) {
+            resp.setStatus(404);
+            return;
+        }
+        Long id = Long.valueOf(pathParts[1]);
+        String blobKey = pathParts[2];
+
+        PhotoService.getInstance().serve(new BlobKey(blobKey), resp);
     }
 }
